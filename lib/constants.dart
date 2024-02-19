1aeb58bf92ba00465bbbac2dffb64b24a55fe37c
ABHISHEK-MISHR <160416802+ABHISHEK-MISHR@users.noreply.github.com>
2024-02-19 23:35:36 +0530

    add files project

---
+++ lib/constants.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  /// red primary
+  static const Color buttonC = Color(0xFFD3190C);
+  // primary blue
+  static const Color blueC = Color(0xFF008DF9);
+
+  /// light grey showing disable
+  static const Color disabledC = Color(0xFFE9E9E9);
+
+  /// dark grey showing disable
+  static const Color disabledDarkC = Color(0xFF494949);
+
+  /// dark grey button color
+  static const Color sButtonDarkC = Color(0xFF494949);
+
+  /// light grey secondary button
+  static const Color sButtonC = Color(0xFFDDDDDD);
+
+  /// black
+  static const Color blackC = Color(0xFF1B1B1B);
+
+  /// light grey bg color
+  static const Color lightGreyBG = Color(0xFFD9D9D9);
+
+  static const Color medGreyBG = Color(0xFF868282);
+
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color lightBlueBgC = Color(0xFFE2F3FF);
+
+  static const Color backgroundRetroBgC = Color(0xFFE9E9E9);
+
+  static const Color gray = Color(0xFFEDEDED);
+
+  static const Color bgQuizItem = Color(0xFF2BAC0B);
+
+  static const Color BgPollItem = Color(0xFFDDDDDD);
+
+
+
+
+}
